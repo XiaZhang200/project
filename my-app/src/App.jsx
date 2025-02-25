ef52cfa16650ff2e8f028d70caded24ce47a9b29
XiaZhang1 <xia.zhang01@estudiant.upf.edu>
2025-02-25 10:47:13 +0100

    fix frontend UI

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -10,16 +10,16 @@ function App() {
         <h1>LoLGPT</h1>
       </header>
 
-      <div>
+      <body>
         <main>
           <ul>
-            <li className="message bot">
-              <span>DeepSeek</span>
-              <span> DALE TETE DAME UN INPUT</span>
+            <li class="message bot">
+              <span>AI</span>
+              <p> DALE TETE DAME UN INPUT</p>
             </li>
-            <li className="message user">  
+            <li class="message user">  
               <span> User</span>
-              <span> Respuesta del user</span>
+              <p> Respuesta del user</p>
             </li>
           </ul>
         
@@ -27,9 +27,9 @@ function App() {
         <form>
             <input name="message" placeholder="Escribe tu mensaje..."/>
             <button>Send</button>
-          </form>
-      </div>
-
+        </form>
+      </body>
+      
     </>
   )
 }
