a74833bdb477f8a23cddceba16472df633523102
XiaZhang1 <xiazhang@Pakito>
2025-02-25 08:40:52 +0100

    hola

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
@@ -8,26 +6,30 @@ function App() {
 
   return (
     <>
+      <header>
+        <h1>LoLGPT</h1>
+      </header>
+
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <main>
+          <ul>
+            <li className="message bot">
+              <span>DeepSeek</span>
+              <span> DALE TETE DAME UN INPUT</span>
+            </li>
+            <li className="message user">  
+              <span> User</span>
+              <span> Respuesta del user</span>
+            </li>
+          </ul>
+        
+        </main>
+        <form>
+            <input name="message" placeholder="Escribe tu mensaje..."/>
+            <button>Send</button>
+          </form>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
     </>
   )
 }
